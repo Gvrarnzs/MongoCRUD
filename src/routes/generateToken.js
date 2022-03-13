1b7d43b9b20684ec29c52acdfbf2ea182b6edffc
Gvrarnzs <arianzas1997@gmail.com>
2022-03-14 00:03:59 +0700

    Minor update

--- src/routes/generateToken.js
+++ src/routes/generateToken.js
@@ -2,6 +2,7 @@ const express = require("express")
 const router = express.Router()
 const jwt = require('jsonwebtoken')
 
+// generate token unprotected
 router.get('/token', (req, res) => {
     const key = process.env.JWT_KEY
     const token = jwt.sign({protect : "protection"}, key, { expiresIn: "12h" })
